9abef68b0a1c03c08858827e029c636fd64bfffb
Ashish-Kumar2309 <109583359+Ashish-Kumar2309@users.noreply.github.com>
2022-07-22 23:21:20 +0530

    Add files via upload

---
+++ even-odd-count.py
@@ -0,0 +1,12 @@
+a=[1,2,3,4,5,6,7,8,9]
+odd=0
+even=0
+for number in a:
+    if (number % 2 == 0):
+        even=even+1
+    else:
+        odd=odd+1
+print("Odd Numbers = ",odd)
+print("Even Numbers = ",even)
+        
+    
---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+# Write a Python program to get the Fibonacci series between 0 to 50
+a=0
+b=1
+c=0
+while c<=50:
+ print(c)
+ a=b
+ b=c
+ c=a+b
+ 
+       
